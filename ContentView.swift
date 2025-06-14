522fe7467f73e280669356f37ce59bb4f6bd4c00
Anto George Jerold <antogeorgejeroldj@gmail.com>
2025-06-14 11:58:30 +0530

    - Update Black and White feature

---
+++ ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  PhotoEditApp
+//
+//  Created by Paranjothi iOS MacBook Pro on 14/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            BlackWhiteFilterView()
+        }
+    }
+}
---
+++ PhotoEditAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditAppApp.swift
+//  PhotoEditApp
+//
+//  Created by Paranjothi iOS MacBook Pro on 14/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
